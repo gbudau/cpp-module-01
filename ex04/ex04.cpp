2cf764f48ab7bab07cae59236096b55382487667
gbudau <54190400+gbudau@users.noreply.github.com>
2021-03-03 00:51:38 +0100

    ADD ex04
    Added ex04

---
+++ ex04/ex04.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int	main() {
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*str_ptr = &str;
+	std::string	&str_ref = str;
+
+	std::cout << *str_ptr << std::endl;
+	std::cout << str_ref << std::endl;
+}
